2fb39eef36266f6c97a9d4967ed1c94c9e0139f3
Thaise <thaise.souza@ebanx.com>
2025-05-25 20:30:56 -0300

    deploy flask gif

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/outro")
+def outro():
+    return render_template("outro.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
